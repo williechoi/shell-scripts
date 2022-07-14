5426aaf3c7ae958d4389345b4fe5907528ed2071
Hyungsuk Choi <choihyungsuk@gmail.com>
2022-07-14 23:30:42 +0900

    exporting variable in shell script

---
+++ exporttest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This is the export test script"
+
+MESSAGE="Hello Linux"
+export MESSAGE
+./secondscript.sh
---
+++ secondscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "This is the second script"
+echo "The message from exporttest is: ${MESSAGE}"
+
